8aa28ad61d5482f227f1a95e587990667f12c475
Khizer Tariq <khizertariq633@gmail.com>
2023-05-14 00:25:07 +0500

    commit to main

---
+++ network.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+:menu
+cls
+echo Network Activity Monitor
+echo =======================
+echo 1. Display current data usage
+echo 2. Display packet loss
+echo 3. Exit
+set /p choice=Enter your choice:
+if "%choice%"=="1" (
+    netstat -e | findstr "Bytes"
+    pause
+    goto menu
+)
+if "%choice%"=="2" (
+    ping -n 10 google.com | findstr "Lost"
+    pause
+    goto menu
+)
+if "%choice%"=="3" (
+    exit
+)
+echo Invalid choice. Press any key to continue...
+pause > nul
+goto menu
