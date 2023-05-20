29ae213755105712af0cb6b1810043f3ecdf732a
Khizer Tariq <khizertariq633@gmail.com>
2023-05-21 02:26:21 +0500

    Fixed Space errors for speak command

--- scripts/speak.bat
+++ scripts/speak.bat
@@ -1,4 +1,13 @@
 @echo off
-if not "%1"=="" (
-    echo %1
+setlocal enabledelayedexpansion
+
+if not "%~1"=="" (
+    set "output=%~1"
+    :loop
+    shift
+    if not "%~1"=="" (
+        set "output=!output! %~1"
+        goto loop
+    )
+    echo %output%
 )
