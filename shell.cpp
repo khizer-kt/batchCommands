ec1dd23b1df68ebdbcff0760abe265bdbbd2d8bd
Khizer Tariq <khizertariq633@gmail.com>
2023-05-21 02:19:00 +0500

    commit cpp code

---
+++ shell.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <windows.h>
+
+const std::vector<std::string> commands = {
+    "cpumanager.bat",
+    "defender.bat",
+    "delete.bat",
+    "filemanager.bat",
+    "filepipe.bat",
+    "files.bat",
+    "firewall.bat",
+    "history.bat",
+    "largestfile.bat",
+    "mem.bat",
+    "multipipe.bat",
+    "myCommands",
+    "network.bat",
+    "process.bat",
+    "search.bat",
+    "speak.bat",
+    "startserver.bat",
+    "userAdmin.bat"};
+
+int main()
+{
+    std::cout << "---MINISHELL---" << std::endl;
+    std::cout << "===============" << std::endl;
+
+    while (true)
+    {
+        std::cout << "Enter a command (or 'exit' to quit): ";
+        std::string command;
+        std::getline(std::cin, command);
+
+        if (command == "exit" || command == "quit")
+            break;
+        bool isValidCommand = false;
+        for (const std::string &cmd : commands)
+        {
+            if (command == cmd)
+            {
+                isValidCommand = true;
+                break;
+            }
+        }
+
+        if (isValidCommand)
+        {
+            std::string fullCommand = "cmd /c " + command;
+            int result = system(fullCommand.c_str());
+
+            if (result == -1 || result == 127)
+                std::cerr << "Execution failed" << std::endl;
+        }
+        else
+        {
+            std::cout << "Invalid command" << std::endl;
+        }
+    }
+    return 0;
+}
--- shell.py
+++ shell.py
@@ -5,10 +5,10 @@ print("---MINISHELL---")
 print("===============")
 while True:
     command = input("Enter a command (or 'exit' to quit): ")
-    if command == "exit" or command== "quit":
+    if command == "exit" or command == "quit":
         break
     if command in commands:
         file_or_function = commands[command]
-        subprocess.call(file_or_function, shell=True)
+        subprocess.call([file_or_function, command], shell=True)
     else:
         print("Invalid command")
