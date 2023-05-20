1efc7a022c70e17a8de3634a1a162dade932b2d0
Khizer Tariq <khizertariq633@gmail.com>
2023-05-21 03:04:51 +0500

    fix speak.bat (AGAIN)

--- scripts/speak.bat
+++ scripts/speak.bat
@@ -1,13 +1,4 @@
 @echo off
-setlocal enabledelayedexpansion
-
-if not "%~1"=="" (
-    set "output=%~1"
-    :loop
-    shift
-    if not "%~1"=="" (
-        set "output=!output! %~1"
-        goto loop
-    )
-    echo %output%
+if not "%1"=="" (
+    echo %1
 )
